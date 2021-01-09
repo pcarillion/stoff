6861087f326910af266dc8659ecca6ec7c63beff
Paul Carillion <pcarillion@yahoo.fr>
2021-01-09 22:32:00 +0100

    de

--- src/components/materiaux.js
+++ src/components/materiaux.js
@@ -17,7 +17,7 @@ const Materiaux = ({materiaux}) => {
                             <h2 className='materiau-component-h2'>{materiau.titre}</h2>
                             <div className='article-info-container'>
                                 <p>par {materiau.auteur}</p>
-                                {materiau.traducteur && <p>traduit de {materiau.langueOriginale}</p>}
+                                {materiau.traducteur && <p>traduit {materiau.langueOriginale}</p>}
                                 {materiau.traducteur && <p>par {materiau.traducteur}</p>}
                                 <p>{materiau.date}</p>
                             </div>
@@ -28,7 +28,7 @@ const Materiaux = ({materiaux}) => {
                             <div className='text-filter'></div>
                             <div className='right-info-container'>
                                 <p>par {materiau.auteur}</p>
-                                {materiau.traducteur && <p>traduit de {materiau.langueOriginale}</p>}
+                                {materiau.traducteur && <p>traduit {materiau.langueOriginale}</p>}
                                 {materiau.traducteur && <p>par {materiau.traducteur}</p>}
                                 <p>{materiau.date}</p>
                             </div>
